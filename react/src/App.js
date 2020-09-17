deba7f6559568ecce025c1d52ac11b77487c1f86
motacapla <t.i.motacapla@gmail.com>
2020-09-18 01:36:34 +0900

    landing page design

--- react/src/App.js
+++ react/src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import TwitterAuth from './components/TwitterAuth';
+import AppCardDeck from './components/AppCardDeck';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image';
 
 // TODO : リファクタしたい、設計全体的に見直し
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <div className="header">
-          <h1>Tutter</h1>
+      <div className="App">
+        <div className="AppHeader">
+          <a href="/"><Image className="Logo" src={`${process.env.PUBLIC_URL}/Tutter-logo-white.png`} /></a>
+          <div className="AppHeaderDescription">
+            <h1>釣った魚と、思い出の一枚を。</h1>
+            <h1>自動生成した魚拓でツイートする</h1>
+          </div>
+          <TwitterAuth />
         </div>
-        <TwitterAuth/>
+        <div className="AppBody">
+          <AppCardDeck />
+        </div>
+        <div className="AppFooter">
+          github
+          twitter
+        </div>
       </div>
     );
   }
 }
- 
+
 export default App;
